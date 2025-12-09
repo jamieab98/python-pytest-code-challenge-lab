2895f136c270f46ecefeab91748a219605383c88
Jamie Bryson <jamieab98@gmail.com>
2025-12-09 15:47:45 -0500

    used the parameterize feature

--- lib/testing/test_palindrome.py
+++ lib/testing/test_palindrome.py
@@ -1,11 +1,14 @@
+import pytest
 from palindrome import longest_palindromic_substring
 
-def test_longest_palindromic_substring():
-    assert longest_palindromic_substring("racecar") == "racecar"
-    assert longest_palindromic_substring("babad") in ("bab", "aba")
-    assert longest_palindromic_substring("apple") == "pp"
-    assert longest_palindromic_substring("dad") == "dad"
-    assert longest_palindromic_substring("") == ""
-    assert longest_palindromic_substring("a") == "a"
-    assert longest_palindromic_substring("asdfghjklkjhgfdsa") == "asdfghjklkjhgfdsa"
-    assert longest_palindromic_substring("abcdefg") in ("a", "b", "c", "d", "e", "f", "g")
+@pytest.mark.parametrize("a, expected", [
+    ("racecar", "racecar"),
+    ("apple", "pp"),
+    ("asdfdsa", "asdfdsa"),
+    ("", ""),
+    ("a", "a"),
+    ("qwertyuioppoiuytrewq", "qwertyuioppoiuytrewq")
+])
+
+def test_longest_palindromic_substring(a, expected):
+    assert longest_palindromic_substring(a) == expected
